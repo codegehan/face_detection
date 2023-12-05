3fbe9f5db47de0ca39fcfadc28542b6bfb284f00
gehan35 <115164221+gehan35@users.noreply.github.com>
2023-12-06 02:27:15 +0800

    Update: fps label

--- attendance.py
+++ attendance.py
@@ -3,17 +3,16 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
+import time
 
 path = 'imageAttendance'
 images = []
 classNames = []
 myList = os.listdir(path)
-#print(myList)
 for cl in myList:
     currentImg = cv2.imread(f'{path}/{cl}')
     images.append(currentImg)
     classNames.append(os.path.splitext(cl)[0])
-#print(classNames)
 def findEncodings(images):
     encodeList = []
     for img in images:
@@ -38,14 +37,18 @@ encodeListKnown = findEncodings(images)
 print('Encoding complete')
 
 #image coming from webcam
-
 cap = cv2.VideoCapture(0)
-
+previousTime = 0
 while True:
     success, img = cap.read()
+    currentTime = time.time()
+    fps = 1 / (currentTime - previousTime)
+    previousTime = currentTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20,30), cv2.FONT_HERSHEY_COMPLEX,0.5,(0,255,0), 2)
+    
     imgSmall = cv2.resize(img, (0,0), None, 0.25, 0.25)
-    imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
-
+    imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)    
+    
     facesCurFrame = face_recognition.face_locations(imgSmall)
     encodeCurFrame = face_recognition.face_encodings(imgSmall, facesCurFrame)
 
@@ -62,8 +65,18 @@ while True:
             y1,x2,y2,x1 =  y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(img, (x1,y1), (x2,y2), (0,0,255), 2)
             cv2.rectangle(img, (x1,y2-20), (x2,y2), (0,0,255), cv2.FILLED)
-            cv2.putText(img, name,(x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255), 2)
+            cv2.putText(img, name,(x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255), 1)
+            markAttendance(name)
+            # Make a post request to send data to database
+            # print(f'Data to send: {name}')
+        else:
+            name = "UNKNOWN"
+            y1,x2,y2,x1 = faceLoc 
+            y1,x2,y2,x1 =  y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img, (x1,y1), (x2,y2), (0,0,255), 2)
+            cv2.rectangle(img, (x1,y2-20), (x2,y2), (0,0,255), cv2.FILLED)
+            cv2.putText(img, name,(x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255), 1)
             markAttendance(name)
 
-    cv2.imshow('Webcome', img)
+    cv2.imshow('Camera', img)
     cv2.waitKey(1)
